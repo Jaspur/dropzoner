9d220947bb88a41134813e10395f5f4ee085fa5c
Jasper Koers <jasper@koers.biz>
2017-04-07 22:26:56 +0200

    not auth

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::post('dropzoner/upload', ['as' => 'dropzoner.upload', 'uses' => 'DropzonerController@postUpload'])->middleware('auth');
-Route::post('dropzoner/delete', ['as' => 'dropzoner.delete', 'uses' => 'DropzonerController@postDelete'])->middleware('auth');
+Route::post('dropzoner/upload', ['as' => 'dropzoner.upload', 'uses' => 'DropzonerController@postUpload']);
+Route::post('dropzoner/delete', ['as' => 'dropzoner.delete', 'uses' => 'DropzonerController@postDelete']);
