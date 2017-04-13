da05a1c774afef0f529e34edba59c2c11a80a304
Jasper Koers <jasper@koers.biz>
2017-04-13 12:40:42 +0200

    serviceprovider

--- src/DropzonerServiceProvider.php
+++ src/DropzonerServiceProvider.php
@@ -10,12 +10,33 @@ class DropzonerServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $packageRoutesPath = __DIR__ . '/Http/routes.php';
+        $packageConfigPath = __DIR__ . '/config/dropzoner.php';
+        $packageViewsPath  = __DIR__ . '/../views';
+        $packageAssetsPath = __DIR__ . '/../assets';
 
-        $this->loadViewsFrom(realpath(__DIR__ . '/../views'), 'dropzoner', __DIR__);
-        $this->setupRoutes($this->app->router);
+        include $packageRoutesPath;
 
-        $this->publishes([__DIR__ . '/config/dropzoner.php' => config_path('dropzoner.php')]);
-        $this->publishes([__DIR__ . '/../assets' => public_path('vendor/dropzoner')], 'public');
+        $config = config_path('dropzoner.php');
+
+        if (file_exists($config)) {
+            $this->mergeConfigFrom($packageConfigPath, 'dropzoner');
+        }
+
+        $this->publishes([
+            $packageConfigPath => $config,
+        ], 'config');
+        $this->loadViewsFrom($packageViewsPath, 'pages');
+
+        $this->publishes([
+            $packageAssetsPath => public_path('vendor/dropzoner'),
+        ], 'public');
+
+        // $this->loadViewsFrom(realpath(__DIR__ . '/../views'), 'dropzoner', __DIR__);
+        // $this->setupRoutes($this->app->router);
+
+        // $this->publishes([__DIR__ . '/config/dropzoner.php' => config_path('dropzoner.php')]);
+        // $this->publishes([__DIR__ . '/../assets' => public_path('vendor/dropzoner')], 'public');
     }
 
     public function setupRoutes(Router $router)
@@ -27,18 +48,19 @@ class DropzonerServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->registerDropzoner();
-        config(['config/dropzoner.php']);
+        $this->app->singleton('dropzoner', function ($app) {
+            return new Dropzoner;
+        });
+
+        // $this->registerDropzoner();
+        // config(['config/dropzoner.php']);
     }
 
     public function registerDropzoner()
     {
-        $this->app->singleton('dropzoner', function ($app) {
-            return new Dropzoner;
+        $this->app->bind('dropzoner', function ($app) {
+            return new Dropzoner($app);
         });
-        // $this->app->bind('dropzoner', function ($app) {
-        //     return new Dropzoner($app);
-        // });
     }
 
     public function provides()
